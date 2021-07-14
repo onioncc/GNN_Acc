66649061f914a4ee4c4102138e50c828953cc519
abhishek115 <apatel653@gatech.edu>
2021-07-13 21:59:57 -0400

    add files

--- GCN/prepare_weights.py
+++ GCN/prepare_weights.py
@@ -48,129 +48,47 @@ def eval(model, device, loader, evaluator):
     input_dict = {"y_true": y_true, "y_pred": y_pred}
     return evaluator.eval(input_dict), y_true, y_pred
 
-print('hi')
-############ Device and test_loader #############
-device = torch.device("cpu")
-dataset = PygGraphPropPredDataset(name='ogbg-molhiv')
-split_idx = dataset.get_idx_split()
-evaluator = Evaluator('ogbg-molhiv')
-test_loader = DataLoader(dataset[split_idx["test"]], batch_size=1, shuffle=False, num_workers=1)
+if __name__ ==  '__main__':
+    ############ Device and test_loader #############
+    device = torch.device("cpu")
+    dataset = PygGraphPropPredDataset(name='ogbg-molhiv')
+    split_idx = dataset.get_idx_split()
+    evaluator = Evaluator('ogbg-molhiv')
+    test_loader = DataLoader(dataset[split_idx["test"]], batch_size=1, shuffle=False, num_workers=1)
 
+    ########### Load the pre-trained GIN model ###########
+    print('Load the pretrained GNN model')
+    model = torch.load('gin_ep1_dim100.pt')
+    """    # model = GNN(gnn_type = 'gin', num_tasks = 1, num_layer = 5, emb_dim = 300, drop_ratio = 0.5, virtual_node = False).to(device)
+        print('Evaluating...')
+        auc, ytrue, ypred = eval(model, device, test_loader, evaluator)
+        print('AUC of the original model:')
+        print(auc)
+    
+        graph_id = 1
+        all_result = {}
+        f_txt = open('Pytorch_output_dim100.txt', 'w+')
+        for yt, yp in zip(ytrue, ypred):
+            # print(yt[0], yp[0])
+            key = 'g' + str(graph_id)
+            all_result[key] = {}
+            all_result[key]['true'] = float(yt[0])
+            all_result[key]['predict'] = float(yp[0])
+            graph_id += 1
+            f_txt.write(key + ': ' + str(yp[0]) + '\n')
+        f_txt.close()
+    
+        f_json = open('all_results_dim100.json', 'w+')
+        json.dump(all_result, f_json)
+        f_json.close()"""
 
+    print('Removing the BatchNorm in the model')
 
-########### Load the pre-trained GIN model ###########
-print('Load the pretrained GNN model')
-model = torch.load('gcn_ep1_dim100.pt')
-#model = GNN(gnn_type = 'gcn', num_tasks = 1, num_layer = 5, emb_dim = 300, drop_ratio = 0.5, virtual_node = False).to(device)
-print('Evaluating...')
-auc, ytrue, ypred = eval(model, device, test_loader, evaluator)
-print('AUC of the original model:')
-print(auc)
+    model_noBN = GNN_noBN(gnn_type='gin', num_tasks=1, num_layer=5, emb_dim=100, drop_ratio=0.5, virtual_node=False).to(
+        device)
 
-########### Collect all the golden outputs from Pytorch ###########
-graph_id = 1
-all_result = {}
-f_txt = open('Pytorch_output_dim100.txt', 'w+')
-for yt, yp in zip(ytrue, ypred):
-    #print(yt[0], yp[0])
-    key = 'g' + str(graph_id)
-    all_result[key] = {}
-    all_result[key]['true'] = float(yt[0])
-    all_result[key]['predict'] = float(yp[0])
-    graph_id += 1
-    f_txt.write(key + ': ' + str(yp[0]) + '\n')
-f_txt.close()
-
-f_json = open('all_results_dim100.json', 'w+')
-json.dump(all_result, f_json)
-f_json.close()
-
-
-
-### FIX THESE STATE_DICTS ###
-
-
-
-########### collecting weights for the accelerator #######################
-
-print('Collecting and reorganizing the weights for the accelerator')
-
-model = torch.load('gcn_ep1_noBN_dim100.pt')
-
-### pack MLP weights
-MLP_weights_0_1 = model.state_dict()['gnn_node_noBN.convs.0.mlp.0.weight'].numpy()
-MLP_weights_1_1 = model.state_dict()['gnn_node_noBN.convs.1.mlp.0.weight'].numpy()
-MLP_weights_2_1 = model.state_dict()['gnn_node_noBN.convs.2.mlp.0.weight'].numpy()
-MLP_weights_3_1 = model.state_dict()['gnn_node_noBN.convs.3.mlp.0.weight'].numpy()
-MLP_weights_4_1 = model.state_dict()['gnn_node_noBN.convs.4.mlp.0.weight'].numpy()
-MLP_1_weights_all = torch.tensor([MLP_weights_0_1, MLP_weights_1_1, MLP_weights_2_1, MLP_weights_3_1, MLP_weights_4_1])
-#print(MLP_1_weights_all.shape)
-
-MLP_weights_0_2 = model.state_dict()['gnn_node_noBN.convs.0.mlp.2.weight'].numpy()
-MLP_weights_1_2 = model.state_dict()['gnn_node_noBN.convs.1.mlp.2.weight'].numpy()
-MLP_weights_2_2 = model.state_dict()['gnn_node_noBN.convs.2.mlp.2.weight'].numpy()
-MLP_weights_3_2 = model.state_dict()['gnn_node_noBN.convs.3.mlp.2.weight'].numpy()
-MLP_weights_4_2 = model.state_dict()['gnn_node_noBN.convs.4.mlp.2.weight'].numpy()
-MLP_2_weights_all = torch.tensor([MLP_weights_0_2, MLP_weights_1_2, MLP_weights_2_2, MLP_weights_3_2, MLP_weights_4_2])
-#print(MLP_2_weights_all.shape)
-
-MLP_bias_0_1 = model.state_dict()['gnn_node_noBN.convs.0.mlp.0.bias'].numpy()
-MLP_bias_1_1 = model.state_dict()['gnn_node_noBN.convs.1.mlp.0.bias'].numpy()
-MLP_bias_2_1 = model.state_dict()['gnn_node_noBN.convs.2.mlp.0.bias'].numpy()
-MLP_bias_3_1 = model.state_dict()['gnn_node_noBN.convs.3.mlp.0.bias'].numpy()
-MLP_bias_4_1 = model.state_dict()['gnn_node_noBN.convs.4.mlp.0.bias'].numpy()
-MLP_1_bias_all = torch.tensor([MLP_bias_0_1, MLP_bias_1_1, MLP_bias_2_1, MLP_bias_3_1, MLP_bias_4_1])
-#print(MLP_1_bias_all.shape)
-
-MLP_bias_0_2 = model.state_dict()['gnn_node_noBN.convs.0.mlp.2.bias'].numpy()
-MLP_bias_1_2 = model.state_dict()['gnn_node_noBN.convs.1.mlp.2.bias'].numpy()
-MLP_bias_2_2 = model.state_dict()['gnn_node_noBN.convs.2.mlp.2.bias'].numpy()
-MLP_bias_3_2 = model.state_dict()['gnn_node_noBN.convs.3.mlp.2.bias'].numpy()
-MLP_bias_4_2 = model.state_dict()['gnn_node_noBN.convs.4.mlp.2.bias'].numpy()
-MLP_2_bias_all = torch.tensor([MLP_bias_0_2, MLP_bias_1_2, MLP_bias_2_2, MLP_bias_3_2, MLP_bias_4_2])
-#print(MLP_2_bias_all.shape)
-
-data = list(MLP_1_weights_all.view(-1).numpy())
-f = open('gcn_ep1_mlp_1_weights_dim100.bin', 'wb')
-packed = struct.pack('f'*len(data), *data)
-f.write(packed)
-f.close()
-
-data = list(MLP_1_bias_all.view(-1).numpy())
-f = open('gcn_ep1_mlp_1_bias_dim100.bin', 'wb')
-packed = struct.pack('f'*len(data), *data)
-f.write(packed)
-f.close()
-
-
-data = list(MLP_2_weights_all.view(-1).numpy())
-f = open('gcn_ep1_mlp_2_weights_dim100.bin', 'wb')
-packed = struct.pack('f'*len(data), *data)
-f.write(packed)
-f.close()
-
-
-data = list(MLP_2_bias_all.view(-1).numpy())
-f = open('gcn_ep1_mlp_2_bias_dim100.bin', 'wb')
-packed = struct.pack('f'*len(data), *data)
-f.write(packed)
-f.close()
-
-# all embeddings
-nd_emb_0 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.0.weight']
-nd_emb_1 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.1.weight']
-nd_emb_2 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.2.weight']
-nd_emb_3 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.3.weight']
-nd_emb_4 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.4.weight']
-nd_emb_5 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.5.weight']
-nd_emb_6 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.6.weight']
-nd_emb_7 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.7.weight']
-nd_emb_8 = model.state_dict()['gnn_node_noBN.atom_encoder.atom_embedding_list.8.weight']
-
-nd_all = torch.cat((nd_emb_0, nd_emb_1, nd_emb_2, nd_emb_3, nd_emb_4, nd_emb_5, nd_emb_6, nd_emb_7, nd_emb_8), dim=0)
-
-data = list(nd_all.view(-1).numpy())
-f = open('gcn_ep1_nd_embed_dim100.bin', 'wb')
-packed = struct.pack('f'*len(data), *data)
-f.write(packed)
-f.close()
+    model_noBN.training = False
+    model_noBN.eval()
+    model.training = False
+    bn_eps = 0.00001
+    print(model_noBN.state_dict().keys())
