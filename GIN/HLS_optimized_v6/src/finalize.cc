f9896d6330bfac8029331847e79dc974d4c084d9
Rishov Sarkar <rishov.sarkar@gatech.edu>
2022-04-06 13:29:03 -0400

    break critical path for global mean pooling

--- GIN/HLS_optimized_v6/src/finalize.cc
+++ GIN/HLS_optimized_v6/src/finalize.cc
@@ -44,10 +44,12 @@ static void global_mean_pooling(
     FM_TYPE sums[EMB_DIM];
 #pragma HLS ARRAY_PARTITION variable=sums cyclic factor=APPLY_PARALLEL dim=1
 
-    int num_iters = ceildiv(num_of_nodes, NODE_PARALLEL);
-    for (int i = 0, nd_base = 0; i < num_iters; i++, nd_base += NODE_PARALLEL)
+    int num_iters = ceildiv(num_of_nodes, NODE_PARALLEL) - 1;
+    int tail_nodes = ((num_of_nodes - 1) % NODE_PARALLEL) + 1;
+
+    global_mean_pooling_main: for (int i = 0; i < num_iters; i++)
     {
-#pragma HLS LOOP_TRIPCOUNT min=ceildiv(ANALYSIS_MIN_NODES, NODE_PARALLEL) max=ceildiv(ANALYSIS_MAX_NODES, NODE_PARALLEL) avg=ceildiv(ANALYSIS_AVG_NODES, NODE_PARALLEL)
+#pragma HLS LOOP_TRIPCOUNT min=(ceildiv(ANALYSIS_MIN_NODES, NODE_PARALLEL) - 1) max=(ceildiv(ANALYSIS_MAX_NODES, NODE_PARALLEL) - 1) avg=(ceildiv(ANALYSIS_AVG_NODES, NODE_PARALLEL) - 1)
         for (int dim_base = 0; dim_base < EMB_DIM; dim_base += APPLY_PARALLEL)
         {
 #pragma HLS PIPELINE II=1
@@ -58,8 +60,6 @@ static void global_mean_pooling(
             for (int nd_offset = 0; nd_offset < NODE_PARALLEL; nd_offset++)
             {
 #pragma HLS UNROLL
-                int nd = nd_base + nd_offset;
-                if (nd == num_of_nodes) break;
                 embeddings[nd_offset] >> embeddings_slice[nd_offset];
             }
 
@@ -72,15 +72,44 @@ static void global_mean_pooling(
                 for (int nd_offset = 0; nd_offset < NODE_PARALLEL; nd_offset++)
                 {
 #pragma HLS UNROLL
-                    int nd = nd_base + nd_offset;
-                    if (nd == num_of_nodes) break;
                     h_graph_el += embeddings_slice[nd_offset][dim_offset];
                 }
 
-                if (nd_base != 0) h_graph_el += sums[dim];
+                if (i != 0) h_graph_el += sums[dim];
                 sums[dim] = h_graph_el;
-                h_graph[dim] = h_graph_el / num_of_nodes;
             }
+        }
+    }
+
+    global_mean_pooling_tail: for (int dim_base = 0; dim_base < EMB_DIM; dim_base += APPLY_PARALLEL)
+    {
+#pragma HLS PIPELINE II=1
+
+        ne_out_t embeddings_slice[NODE_PARALLEL];
+#pragma HLS ARRAY_PARTITION variable=embeddings_slice complete dim=1
+
+        for (int nd_offset = 0; nd_offset < NODE_PARALLEL; nd_offset++)
+        {
+#pragma HLS UNROLL
+            if (nd_offset == tail_nodes) break;
+            embeddings[nd_offset] >> embeddings_slice[nd_offset];
+        }
+
+        for (int dim_offset = 0; dim_offset < APPLY_PARALLEL; dim_offset++)
+        {
+#pragma HLS UNROLL
+            int dim = dim_base + dim_offset;
+            FM_TYPE h_graph_el = 0;
+
+            for (int nd_offset = 0; nd_offset < NODE_PARALLEL; nd_offset++)
+            {
+#pragma HLS UNROLL
+                if (nd_offset == tail_nodes) break;
+                h_graph_el += embeddings_slice[nd_offset][dim_offset];
+            }
+
+            if (num_iters != 0) h_graph_el += sums[dim];
+            h_graph[dim] = h_graph_el / num_of_nodes;
         }
     }
 }
