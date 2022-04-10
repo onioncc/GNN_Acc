55abf6abe2b91a982ce11563ea03693301660a7a
Rishov Sarkar <rishov.sarkar@gatech.edu>
2022-04-10 01:44:26 -0400

    prepare for PNA U50 build

---
+++ PNA/PNA_HLS_U50/v2/src/host.h
@@ -0,0 +1,34 @@
+#ifndef __HOST_H__
+#define __HOST_H__
+
+#include "dcl.h"
+#include "xcl2.hpp"
+
+constexpr int NUM_GRAPHS = 4113;
+constexpr int NUM_TRIALS = 100;
+
+template <typename T>
+using aligned_vector = std::vector<T, aligned_allocator<T>>;
+
+extern aligned_vector<WT_TYPE> node_embedding_weight_fixed;
+extern aligned_vector<WT_TYPE> node_conv_weights_fixed;
+extern aligned_vector<WT_TYPE> node_conv_bias_fixed;
+extern aligned_vector<WT_TYPE> graph_mlp_1_weights_fixed;
+extern aligned_vector<WT_TYPE> graph_mlp_1_bias_fixed;
+extern aligned_vector<WT_TYPE> graph_mlp_2_weights_fixed;
+extern aligned_vector<WT_TYPE> graph_mlp_2_bias_fixed;
+extern aligned_vector<WT_TYPE> graph_mlp_3_weights_fixed;
+extern aligned_vector<WT_TYPE> graph_mlp_3_bias_fixed;
+extern aligned_vector<WT_TYPE> avg_deg_fixed;
+
+void load_weights();
+void fetch_one_graph(
+    int g,
+    char* graph_name,
+    aligned_vector<node_feature_t>& node_feature,
+    aligned_vector<edge_t>& edge_list,
+    int num_of_nodes,
+    int num_of_edges
+);
+
+#endif
