2c03b80b3b7be8ced02bbb3b431096bd1983f58a
Rishov Sarkar <rishov.sarkar@gatech.edu>
2022-04-17 23:44:15 -0400

    update host code to not depend on hls_vector.h

--- GAT/GAT_U50/HLS_test_v3/src/host.h
+++ GAT/GAT_U50/HLS_test_v3/src/host.h
@@ -1,8 +1,43 @@
 #ifndef __HOST_H__
 #define __HOST_H__
 
-#include "dcl.h"
+#include <array>
+#include <ap_fixed.h>
 #include "xcl2.hpp"
+
+// #region Model Parameters
+constexpr int MAX_EDGE = 500;
+constexpr int MAX_NODE = 500;
+constexpr int ND_FEATURE = 9;
+constexpr int ND_FEATURE_TOTAL = 173;
+constexpr int EDGE_ATTR = 3;
+constexpr int ED_FEATURE_PER_LAYER = 13;
+constexpr int EMB_DIM = 16;
+constexpr int NUM_HEADS = 4;
+constexpr int NUM_LAYERS = 5;
+constexpr int NUM_TASK = 1;
+// #endregion
+
+// #region Hardware Parameters
+constexpr int GATHER_PARALLEL = 8; // how many dimensions of EMB_DIM should a message passing PE process each cycle?
+constexpr int APPLY_PARALLEL = 1; // how many dimensions of EMB_DIM should the node embedding PE process each cycle?
+constexpr int NODE_PARALLEL = 2; // how many nodes should the node embedding PE process simultaneously?
+constexpr int EDGE_PARALLEL = 4; // how many message passing PEs are there?
+constexpr int MLP_PARALLEL = 2;
+// #endregion
+
+// #region Data Types
+typedef ap_fixed<16, 6> FM_TYPE;
+typedef ap_fixed<16, 6> WT_TYPE;
+
+typedef std::array<int, ND_FEATURE> node_feature_t;
+typedef std::array<int, EDGE_ATTR> edge_attr_t;
+
+typedef struct {
+    int u;
+    int v;
+} edge_t;
+// #endregion
 
 constexpr int NUM_GRAPHS = 4113;
 constexpr int NUM_TRIALS = 100;
